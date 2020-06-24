1d8133cd6af808efe07ad0b1aa1a81318863b46a
WaseemWebDev <waseemyousafzai520@gmail.com>
2020-06-24 01:04:30 -0700

    Complete Todo App
    completed todo app

--- src/list.js
+++ src/list.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 
 function List(props) {
     let [line, setLine] = useState(false)
+
     const cutIt = () => {
         setLine(line = !line);
     }
+
     return (
-        <div>
-            <li style={{ textDecoration: line ? "line-through" : "none" ,fontSize:"20px"}}> {props.item} </li> 
-            <span> <button onClick={() => {
-                props.onSelect(props.id)
-            }}>D</button></span><button onClick={cutIt}>complete</button>
+        <div style={{ padding: "5px" }}>
+
+            <li >
+
+                <input ref={props.ref} className="lists" autocomplete="off" style={{ textDecoration: line ? "line-through" : "none", fontWeight: "bold" }} type="text" id={props.id} value={props.item} onChange={(e) => {
+                    props.setUpdate(e, props.id)
+                }} /> <span onClick={props.focusInput} className="edit">Edit</span> 
+                <span style={{ marginLeft: "200px" }}><button onClick={() => {
+                    props.onSelect(props.id)
+                }}>Delete
+                </button></span><button onClick={cutIt}>complete</button> </li>
+
+
 
         </div>
     );
--- src/todo.js
+++ src/todo.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import List from './list';
 
 function Todo() {
     const [todo, setTodo] = useState([]);
-    const [text, setText] = useState("");
+    let [text, setText] = useState("");
+    const inputRef = useRef();
+
 
     const addItem = () => {
         if (text === "") {
@@ -13,34 +15,71 @@ function Todo() {
             setTodo([
                 ...todo,
                 {
-                    // id: todo.length,
                     value: text
                 },
-
             ]);
             setText("");
-
         }
 
     }
 
+
+
+
+    // const findItem = id => {
+    //     const item = todo.find(task => task.id === id)
+
+    //     setEditItem(item)
+    // }
+    // const editTask = (title, id) => {
+    //     const newTasks = tasks.map(task => (task.id === id ? { text, id } : task))
+    //     setTodo(newTasks)
+    //     setEditItem(null)
+    // }
+
     let deleteItem = (index) => {
         const newTodos = [...todo];
-             newTodos.splice(index, 1);
-             setTodo(newTodos);
+        newTodos.splice(index, 1);
+        setTodo(newTodos);
+    }
+
+    const clearTodo = () => {
+        setTodo([]);
+    }
+
+    function setUpdate(e, i) {
+        //   let newItem = [...todo]
+        //   newItem.map(function (task, index) {
+        //      if (index === i) {
+        //    task.value = e.target.value
+        //     }
+        //     setTodo(newItem);
+        // });
+        const newTodos = [...todo];
+        newTodos[i].value = e.target.value
+        setTodo(newTodos);
+        console.log(newTodos)
+    }
+
+    function focusInput() {
+        inputRef.current.focus();
     }
 
 
     return (
         <>
-        <div className="main-frame">
-            <input type="text" value={text} onChange={e => setText(e.target.value)} />
-            <button onClick={addItem}>Add a number</button>
-            <ul>
-                {todo.map((item, index) => {
-                    return <List item={item.value} key={index} id={index} onSelect={deleteItem} />
-                })}
-            </ul>
+            <div className="main-frame" >
+
+                <input type="text" value={text} onChange={e => setText(e.target.value)} />
+                <button onClick={addItem}>Add a number</button>
+                <button onClick={clearTodo}>clear</button>
+                <br />
+                <ol>
+                    {todo.map((item, index) => {
+                        return <List item={item.value} key={index} id={index} onSelect={deleteItem} setUpdate={setUpdate} focusInput={focusInput} ref={inputRef} />
+                    })}
+                </ol>
+                {todo.length ? <span style={{ fontWeight: "bold", color: `${todo.length ? "green" : "red"}` }}>Total todos {todo.length}</span> : <h3 style={{ color: "red" }}>No Items Found</h3>}
             </div>
         </>
     );
